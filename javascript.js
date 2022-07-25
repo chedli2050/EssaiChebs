38b3ea582faf87d59c76c14a4eef7dd83528f813
chedli2050 <109752638+chedli2050@users.noreply.github.com>
2022-07-25 01:57:51 +0100

    Add files via upload

--- javascript.js
+++ javascript.js
@@ -28,3 +28,45 @@ function retirermenu(){
     
 }
 logobarrehaut.addEventListener('click',retirermenu);
+
+var d = new Date();
+
+    var annee = d.getFullYear();
+    var mois = d.getMonth()+1;
+    var jour = d.getDate();
+    var heure = d.getHours();
+    var minute = d.getMinutes();
+    var seconde = d.getSeconds();
+
+var moisl = ''
+
+if(mois == 1){moisl="Janiver"}
+else if (mois == 2){moisl="Jévrier"}
+else if (mois == 3){moisl="Mars"}
+else if (mois == 4){moisl="Avril"}
+else if (mois == 5){moisl="Mai"}
+else if (mois == 6){moisl="Juin"}
+else if (mois == 7){moisl="Juillet"}
+else if (mois == 8){moisl="Aout"}
+else if (mois == 9){moisl="Septembre"}
+else if (mois == 10){moisl="octobre"}
+else if (mois == 11){moisl="Novembre"}
+else if (mois == 12){moisl="Décembre"};
+
+
+
+
+
+var ladate = document.getElementById('ladate');
+ladate.innerHTML =  jour + " " + moisl + " " + annee;
+
+var boxblocbt = document.getElementById('boxblocbt');
+var boxlivresbt = document.getElementById('boxlivresbt');
+var boxflyersbt = document.getElementById('boxflyersbt');
+var boxportedocbt = document.getElementById('boxportedocbt');
+
+function essai(){alert('oooooooooooooo')};
+boxblocbt.addEventListener('click',essai);
+boxlivresbt.addEventListener('click',essai);
+boxflyersbt.addEventListener('click',essai);
+boxportedocbt.addEventListener('click',essai);
